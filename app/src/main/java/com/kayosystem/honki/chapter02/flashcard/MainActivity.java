28cd24f7f1e6a173cb86b7d3956874bba7934e58
SusaHitomi <a17dc092@dhw.ac.jp>
2018-04-21 19:06:40 +0900

    flashcard

--- app/src/main/java/com/kayosystem/honki/chapter02/flashcard/MainActivity.java
+++ app/src/main/java/com/kayosystem/honki/chapter02/flashcard/MainActivity.java
@@ -21,44 +21,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dog = new Word(R.drawable.dog,"犬");
-        cat = new Word(R.drawable.cat,"猫");
-        bear = new Word(R.drawable.bear,"熊");
-
         container = (LinearLayout) findViewById(R.id.container);
 
-        //container.addView(nameTextView);
+        Word dog = new Word(R.drawable.dog, "犬");
+        Word cat = new Word(R.drawable.cat, "猫");
+        Word bear = new Word(R.drawable.bear, "熊");
 
+        addWord(dog);
+        addWord(cat);
+        addWord(bear);
+
+
+    }
+
+    public void addWord(Word word) {
         LinearLayout layout = new LinearLayout(this.getApplicationContext());
         layout.setOrientation(LinearLayout.HORIZONTAL);
 
         TextView nameTextView = new TextView(getApplicationContext());
-        nameTextView.setText(dog.name);
+        nameTextView.setText(word.name);
 
         ImageView imageView = new ImageView(this);
-        imageView.setImageResource(dog.resId);
+        imageView.setImageResource(word.resId);
 
         layout.addView(imageView);
         layout.addView(nameTextView);
 
         container.addView(layout);
-
-
-
-
-        LinearLayout layout2 = new LinearLayout(this.getApplicationContext());
-        layout2.setOrientation(LinearLayout.HORIZONTAL);
-
-        TextView nameTextView2 = new TextView(getApplicationContext());
-        nameTextView2.setText(cat.name);
-
-        ImageView imageView2 = new ImageView(this);
-        imageView2.setImageResource(cat.resId);
-
-        layout2.addView(imageView2);
-        layout2.addView(nameTextView2);
-
-        container.addView(layout2);
 
     }
 }
